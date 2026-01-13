e0140334ca0e8c473505ba83722a378dc3dc3dfe
tilolab-dev <tilolab.dev@gmail.com>
2026-01-13 17:15:43 +0200

    small changes

--- components/IndexCategories.vue
+++ components/IndexCategories.vue
@@ -58,8 +58,6 @@ onMounted(async () => {
 
     if (getCategories && getCategories.data) {
       categories.value = getCategories.data;
-
-      console.log(categories.value);
     }
   } catch (err) {
     console.log(err);
