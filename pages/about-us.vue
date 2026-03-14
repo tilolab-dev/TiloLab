7b79abd0ac50b0153cb93cba4b23b375382e1bb9
tilolab-dev <tilolab.dev@gmail.com>
2026-03-14 18:26:12 +0200

    test about us page

--- pages/about-us.vue
+++ pages/about-us.vue
@@ -75,7 +75,9 @@
 </template>
 
 <script setup>
-import { useSeoMeta } from "#imports";
+import { useSeoMeta, useRouter } from "#imports";
+const router = useRouter();
+
 useSeoMeta({
   title: "Про нас",
   description:
@@ -83,7 +85,10 @@ useSeoMeta({
   ogTitle: "Про нас",
   ogDescription:
     "Tilo Lab — це сучасний простір дослідження інтимного здоров’я, задоволення та тілесної гармонії. Ми створили лабораторію, де інновації, наука та турбота про тіло поєднуються у відповідальний та делікатний сервіс.",
-  ogImage: "https://tilolab.com.ua/images/about-main.webp"
+  ogImage: "https://tilolab.com.ua/images/about-main.webp",
+
+  ogType: "website",
+  ogUrl: router.currentRoute.value.fullPath
 });
 </script>
 
